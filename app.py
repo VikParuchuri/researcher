6fef5fba8c6490f437b009b05155025f35108136
Vik Paruchuri <vik.paruchuri@gmail.com>
2022-12-23 16:20:04 -0800

    Make responses streaming

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import time
 from urllib.parse import urlparse
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, stream_with_context
 
 import settings
 from filter import filter_links
@@ -17,6 +17,27 @@ app = Flask(__name__)
 def show_search_form():
     return render_template("index.html", placeholder="Enter search query")
 
+def render_search_results(links=None, chunks=None, summary=None, template="results.html"):
+    link_data = None
+    chunk_data = None
+    if links:
+        link_data = []
+        for link in links:
+            link_data.append({
+                "link": link.link,
+            })
+    if chunks:
+        chunk_data = []
+        for i, chunk in enumerate(chunks):
+            chunk_data.append({
+                "title": chunk.title,
+                "text": chunk.text[:settings.CHUNK_DISPLAY_CHARS] + "...",
+                "rank": i + 1,
+                "link": chunk.link,
+                "hostname": urlparse(chunk.link).hostname
+            })
+    return render_template(template, links=link_data, results=chunk_data, summary=summary)
+
 
 def run_search(query):
     if len(query) < settings.QUERY_MIN_LENGTH:
@@ -28,32 +49,25 @@ def run_search(query):
     print(f"Search runtime: {search_time - start}")
 
     results = filter_links(results)
+    yield render_search_results(links=results, template="links.html")
+
     chunks = find_likely_chunks(results, query)
 
     processing_time = time.time()
     print(f"Local processing runtime: {processing_time - search_time}")
+    yield render_search_results(chunks=chunks, template="sources.html")
 
     summary_text = get_summary(query, chunks)
 
+    yield render_search_results(summary=summary_text, template="summary.html")
     print(f"API runtime: {time.time() - processing_time}")
-    template_results = []
-    for i, chunk in enumerate(chunks):
-        data = {
-            "title": chunk.title,
-            "text": chunk.text[:settings.CHUNK_DISPLAY_CHARS] + "...",
-            "rank": i + 1,
-            "link": chunk.link,
-            "hostname": urlparse(chunk.link).hostname
-        }
-        template_results.append(data)
-    return render_template("index.html", results=template_results, placeholder="", summary=summary_text, query=query)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def search_form():
     if request.method == 'POST':
         query = request.form["query"]
-        return run_search(query)
+        return stream_with_context(run_search(query)), {"Content-Type": "text/html"}
     else:
         return show_search_form()
 
